ee9d153ee005fb5a218bb3a5b5445c161e16e3aa
danoro <danielorozcomoya@gmail.com>
2021-10-18 17:54:26 -0500

    Add files and configurations to enable microservices

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,14 @@
 var express = require('express'),
-path = require('path'),
-bodyParser = require('body-parser'),
-cors = require('cors'),
-mongoose = require('mongoose');
+  path = require('path'),
+  bodyParser = require('body-parser'),
+  cors = require('cors'),
+  mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/mean', { useNewUrlParser: true }).then(
-  () => {console.log('Database is connected') },
-  err => { console.log('Can not connect to the database'+ err)});
+
+//URI: mongodb://$dataBaseContainer:27017/mean
+mongoose.connect('mongodb://devopsapps_database_1:27017/mean', { useNewUrlParser: true }).then(
+  () => { console.log('Database is connected') },
+  err => { console.log('Can not connect to the database' + err) });
 
 const userRoute = require('./routes/user.route');
 var app = express();
@@ -14,10 +16,10 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use('/user', userRoute);
-app.get('/', function(req, res){
-   res.send("Hello World!");
+app.get('/', function (req, res) {
+  res.send("Hello World!");
 });
 
-app.listen(3000,function(){
-    console.log('Listening on port 3000!');
+app.listen(3000, function () {
+  console.log('Listening on port 3000!');
 });
